01b54e6ae30fe80ebd9c1ae537f25da1df11a60b
ivanhack34 <ivanpolanco34@gmail.com>
2022-10-23 04:20:49 -0400

    Parte 5 cart y parte de purchaser

--- src/components/home/CardProduct.jsx
+++ src/components/home/CardProduct.jsx
@@ -1,10 +1,15 @@
+import axios from 'axios'
 import React from 'react'
+import { useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
+import { getAllProductsCart } from '../../store/slices/cart.slice'
+import getConfig from '../../utils/getConfig'
 import './styles/cardProduct.css'
 
 const CardProduct = ({product}) => {
 
   const navigate = useNavigate()
+  const dispatch = useDispatch()
 
   const handleNavigation = () => {
     navigate(`/product/${product.id}`)
@@ -12,6 +17,17 @@ const CardProduct = ({product}) => {
 
   const handleAddCart = e => {
     e.stopPropagation()
+    const URL = 'https://ecommerce-api-react.herokuapp.com/api/v1/cart'
+    const data = {
+      id: product.id,
+      quantity: 1
+    }
+    axios.post(URL,data, getConfig())
+      .then(res => {
+        console.log(res.data)
+        dispatch(getAllProductsCart())
+      })
+      .catch(err => console.log(err))
   }
 
   return (
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,8 +1,52 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import CartProduct from '../components/cart/CartProduct'
+import { getAllProductsCart, setCartGlobal } from '../store/slices/cart.slice'
+import getConfig from '../utils/getConfig'
 
 const Cart = () => {
+
+  const cart = useSelector(state => state.cart)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getAllProductsCart())
+  }, [])
+  
+  console.log(cart);
+
+  const handlePurchase = () => {
+    const URL = 'https://ecommerce-api-react.herokuapp.com/api/v1/purchases'
+    const data = {
+      street: 'Esquina h5',
+      colony: 'Distrito Nacional',
+      zipCode: 10506,
+      city: "Santo Domingo",
+      reference: "Some reference"
+    }
+    axios.post(URL, data, getConfig())
+      .then(res => {
+        console.log(res.data)
+        dispatch(setCartGlobal(null))
+      })
+      .catch(err => console.log(err))
+  }
+
   return (
-    <div>Cart</div>
+    <div className='cart'>
+      <div className="cart__container">
+        {
+          cart?.products.map(product => (
+            <CartProduct
+              key={product.id}
+              product={product}
+            />
+          ))
+        }
+      </div>
+      <button onClick={handlePurchase} style={{fontSize: '30px'}}>Buy Now</button>
+    </div>
   )
 }
 
