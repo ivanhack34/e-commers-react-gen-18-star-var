4cc6cbe81ad9704ed4a40005697191ea38a070e6
ivanhack34 <ivanpolanco34@gmail.com>
2022-10-23 03:25:58 -0400

    parte 4 login

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import axios from 'axios'
 import { useEffect } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Header from './components/shared/Header'
 import Cart from './pages/Cart'
 import Home from './pages/Home'
 import LoginScreen from './pages/LoginScreen'
@@ -35,12 +36,13 @@ function App() {
 
   return (
     <div className="App">
+      <Header />
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/product/:id' element={<ProductId />}/>
         <Route path='/login' element={<LoginScreen />} />
 
-        <Route element={<ProtectedRoutes/>}>
+        <Route element={<ProtectedRoutes />}> 
           <Route path='/cart' element={<Cart/>}/>
           <Route path='/purchases' element={<Purchases/>}/>
         </Route>
